c822ee00fa0e8e2073bed97a6e8240127374344a
dukecaboom <kidkakashi13@gmail.com>
2020-07-14 19:19:38 -0700

    Add problem 22

---
+++ Problem_22.py
@@ -0,0 +1,14 @@
+with open("p022_names.txt") as reader:
+    names = reader.read().replace("\"", "").split(",")
+
+names.sort()
+
+total_name_score = 0
+
+# names = ['COLIN']
+
+for pos in range(len(names)):
+    name_score = sum([ord(i)-64 for i in names[pos]]) * (pos+1)
+    total_name_score += name_score
+
+print(total_name_score)
