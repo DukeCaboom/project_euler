b69fb11ee215bac0a44121a7ab31bc3ed422de31
dukecaboom <kidkakashi13@gmail.com>
2020-07-24 18:10:17 -0700

    Fix Problem 32

--- Problem_32.py
+++ Problem_32.py
@@ -6,16 +6,16 @@ def digits_repeated(numbers):
 
 
 for i in range(1, 9999):
-    if (not digits_repeated([n for n in str(i)])):
+    if (not digits_repeated([n for n in str(i)])) and '0' not in str(i):
         for j in range(1, 9999):
-            if ((not digits_repeated([n for n in str(j)])) and '0' not in str(j)) :
+            if ((not digits_repeated([n for n in str(j)]))) and '0' not in str(j):
                 product = i*j
-                print(f"i:{i} j:{j} p:{product}")
                 if ((not digits_repeated([n for n in str(product)])) and '0' not in str(product)):
                     mmp = str(i) + str(j) + str(product)
-                    if len(str(mmp)) != 9:
+                    if len(mmp) > 9:
                         break
-                    if not digits_repeated([n for n in mmp]) and product not in pandigital_products:
+                    if len(mmp) == 9 and (not digits_repeated([n for n in mmp])) and (product not in pandigital_products):
+                        print(f"i:{i} j:{j} p:{product}")
                         pandigital_products.append(product)
 
 
