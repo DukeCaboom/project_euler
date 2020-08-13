56637920162f3e5fe9806a565f6a3d831a9103a8
dukecaboom <kidkakashi13@gmail.com>
2020-08-13 16:12:20 -0700

    improvements for Problem_1

--- Problem_1.py
+++ Problem_1.py
@@ -1,6 +1,12 @@
 if __name__ == "__main__":
-    sum = 0
-    for i in range(1000):
-        if i%3 == 0 or i%5 == 0:
-            sum = sum + i
-    print(sum)
+    # sum = 0
+    list_5 = []
+    list_3 = []
+    for i in range(0, 100, 5):
+        if i%5 == 0:
+            list_5.append(i)
+    for j in range(100):
+        if j%3 == 0:
+            list_3.append(j)
+    print(list(set(list_3 + list_5)))
+    print(sum(list(set(list_3 + list_5))))
